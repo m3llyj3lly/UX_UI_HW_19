47c78b4a4ea237768cd2bb291b32079315c47f1a
m3llyj3lly <melissa.young7@gmail.com>
2022-03-11 23:36:09 -0500

    Basic structure

---
+++ js/index.js
@@ -0,0 +1,16 @@
+console.log("Your index.js file is loaded correctly!");
+function mouseOver(x) {
+    x.style.color = "orange";
+   }
+   function mouseOut(x) {
+     x.style.color = "#FFE600";
+   }
+   function bigImg(x) {
+    x.style.height = "500px";
+    x.style.width = "500px";
+  }
+  
+  function normalImg(x) {
+    x.style.height = "300px";
+    x.style.width = "333px";
+  }
